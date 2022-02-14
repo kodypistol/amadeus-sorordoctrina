c28fa1cb60313a6be4c4f644172672649dd5e637
Ckyre <ckyre.game@gmail.com>
2022-02-14 15:55:31 +0100

    onload event

--- src/uiManager.js
+++ src/uiManager.js
@@ -1,11 +1,31 @@
 import signal from "signal-js";
+import router from "./router";
+
+// DOM references
+const landingSection = document.querySelector("#landing-section");
+const loadingSection = document.querySelector("#loading-section");
 
 const uiManager = {
     init(){
+        // Events
         signal.on('changeScreen', this.onChangeScreen);
+        landingSection.addEventListener("click", this.onLoadingQuit);
     },
 
     onChangeScreen(index){
+        switch(index){
+            case 0:
+                landingSection.classList.add("active");
+                loadingSection.classList.remove("active");
+            case 1:
+                landingSection.classList.remove("active");
+                loadingSection.classList.add("active");
+
+        }
+    },
+
+    onLoadingQuit(){
+        router.showScreen(1);
     }
 }
 
