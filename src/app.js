112f5f65e3d9061c7b730cfc29562e1a40b7b692
NicoooM <nmarsan33000@gmail.com>
2022-02-15 17:32:48 +0100

    add audio manager

--- src/app.js
+++ src/app.js
@@ -1,20 +1,19 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 import "./styles/styles.scss";
 
-import router from './router';
-import ui from './uiManager';
-import scene from './scene';
-import renderer from './renderer';
-
+import router from "./router";
+import ui from "./uiManager";
+import scene from "./scene";
+import renderer from "./renderer";
+import audioManager from "./audioManager";
 
 // Point d'entrée
 router.init();
-window.addEventListener("load", () => 
-{
-    ui.init();
-    //experience.init();
-
-    router.showScreen(0);
+window.addEventListener("load", () => {
+  ui.init();
+  //experience.init();
+  router.showScreen(3);
+  audioManager.init();
 });
 
 // Experience Manager
@@ -25,42 +24,42 @@ scene.create(canvas);
 
 // Texture loader
 const textureLoader = new THREE.TextureLoader();
-const brickTexture = textureLoader.load('./textures/color.jpg')
-const brickNormalTexture = textureLoader.load('./textures/normal.jpg')
+const brickTexture = textureLoader.load("./textures/color.jpg");
+const brickNormalTexture = textureLoader.load("./textures/normal.jpg");
 
 // Fill scene with objects
 const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
 
 scene.addObject(cube);
 
 const rectangle1 = new THREE.Mesh(
-    new THREE.BoxBufferGeometry(2, 4, 10, 20, 20),
-    new THREE.MeshBasicMaterial({
-        map: brickTexture,
-        normalMap: brickNormalTexture,
-        transparent: true,
-        opacity: 0,
-        name: 'Bridge'
-    })
+  new THREE.BoxBufferGeometry(2, 4, 10, 20, 20),
+  new THREE.MeshBasicMaterial({
+    map: brickTexture,
+    normalMap: brickNormalTexture,
+    transparent: true,
+    opacity: 0,
+    name: "Bridge",
+  })
 );
-rectangle1.position.x = 0
-rectangle1.position.y = 0
-rectangle1.position.z = 0
+rectangle1.position.x = 0;
+rectangle1.position.y = 0;
+rectangle1.position.z = 0;
 
 scene.addObject(rectangle1);
 
 const rectangle2 = new THREE.Mesh(
-    new THREE.BoxBufferGeometry(2, 4, 10, 20, 20),
-    new THREE.MeshBasicMaterial({
-        map: brickTexture,
-        normalMap: brickNormalTexture,
-        transparent: true,
-        opacity: 0,
-        name: 'Bridge2'
-    })
+  new THREE.BoxBufferGeometry(2, 4, 10, 20, 20),
+  new THREE.MeshBasicMaterial({
+    map: brickTexture,
+    normalMap: brickNormalTexture,
+    transparent: true,
+    opacity: 0,
+    name: "Bridge2",
+  })
 );
 scene.addObject(rectangle2);
 
@@ -70,20 +69,19 @@ renderer.init(canvas);
 const clock = new THREE.Clock();
 let lastElapsedTime = 0;
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - lastElapsedTime
-    lastElapsedTime = elapsedTime
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - lastElapsedTime;
+  lastElapsedTime = elapsedTime;
 
-    // Update controls
-    scene.getCamera().update();
+  // Update controls
+  scene.getCamera().update();
 
-    // Render
-    renderer.draw(scene);
+  // Render
+  renderer.draw(scene);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick);
-}
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
 tick();
--- src/uiManager.js
+++ src/uiManager.js
@@ -4,30 +4,70 @@ import router from "./router";
 // DOM references
 const loadingSection = document.querySelector("#loading-section");
 const landingSection = document.querySelector("#landing-section");
+const s2 = document.querySelector("#s2");
+const s3 = document.querySelector("#s3");
+const s4 = document.querySelector("#s4");
+const s5 = document.querySelector("#s5");
+const s6 = document.querySelector("#s6");
+const s7 = document.querySelector("#s7");
+const s8 = document.querySelector("#s8");
+const s9 = document.querySelector("#s9");
 
 const uiManager = {
-    init(){
-        // Events
-        signal.on('changeScreen', this.onChangeScreen);
-        loadingSection.addEventListener("click", this.onLoadingQuit);
-    },
+  init() {
+    // Events
+    signal.on("changeScreen", this.onChangeScreen);
+    loadingSection.addEventListener("click", this.onLoadingQuit);
+  },
 
-    onChangeScreen(index){
-        
-        switch(index){
-            case 0:
-                loadingSection.classList.add("active");
-                landingSection.classList.remove("active");
-            case 1:
-                loadingSection.classList.remove("active");
-                landingSection.classList.add("active");
-
-        }
-    },
-
-    onLoadingQuit(){
-        router.showScreen(1);
+  onChangeScreen(index) {
+    switch (index) {
+      case 0:
+        loadingSection.classList.add("active");
+        landingSection.classList.remove("active");
+        break;
+      case 1:
+        loadingSection.classList.remove("active");
+        landingSection.classList.add("active");
+        break;
+      case 2:
+        s2.classList.add("active");
+        landingSection.classList.remove("active");
+        break;
+      case 3:
+        s2.classList.remove("active");
+        s3.classList.add("active");
+        break;
+      case 4:
+        s4.classList.add("active");
+        s3.classList.remove("active");
+        break;
+      case 5:
+        s4.classList.remove("active");
+        s5.classList.add("active");
+        break;
+      case 6:
+        s6.classList.add("active");
+        s5.classList.remove("active");
+        break;
+      case 7:
+        s6.classList.remove("active");
+        s7.classList.add("active");
+        break;
+      case 8:
+        s8.classList.add("active");
+        s7.classList.remove("active");
+        break;
+      case 9:
+        s8.classList.remove("active");
+        s9.classList.add("active");
+        break;
     }
-}
+  },
+
+  onLoadingQuit() {
+    router.showScreen(1);
+  },
+};
 
 export default uiManager;
